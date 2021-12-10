dbe6bbb5a4d8f33b6c8d2bbc806714d18c52f765
oldfartdeveloper <scottnelsonsmith@gmail.com>
2021-12-10 09:20:01 -0800

    Have foldl routine compiling

--- src/Main.purs
+++ src/Main.purs
@@ -1,7 +1,15 @@
 module Main where
 
 import Prelude
-import Data.List (List(..), catMaybes, fromFoldable, (:))
+import Data.Foldable (foldl)
+import Data.List
+  ( List(..)
+  , catMaybes
+  , drop
+  , fromFoldable
+  , singleton
+  , (:)
+  )
 import Data.Int (fromString)
 import Data.Maybe (Maybe, fromJust)
 import Data.String.Utils (lines)
@@ -16,14 +24,30 @@ import Partial.Unsafe (unsafePartial)
 main :: Effect Unit
 main = do
   contents <- readTextFile ASCII "src/File1.txt"
-  log $ show $ day1Part2 $ getNumbers contents
+  log $ show $ day1 $ getNumbers contents
+  -- log $ show $ day1Part2 $ getNumbers contents
 
 getNumbers :: String -> List Int
 getNumbers contents =
   catMaybes $ map fromString $ fromFoldable $ lines contents
 
 day1Part2 :: List Int -> Int
-day1Part2 xs = day1 xs
+day1Part2 xs =
+  let
+    token =
+      Token
+        { x3 : drop 3 xs
+        , x : xs
+        , tripletts : Nil
+        }
+    _ = foldl buildTriplets (singleton token) sample
+  in
+    42
+
+data Token = Token { x3 :: List Int, x :: List Int, tripletts :: List Int }
+
+buildTriplets :: List Token -> Int -> List Token
+buildTriplets tokens x = tokens
 
 day1 :: List Int -> Int
 day1 Nil = 0
@@ -43,8 +67,8 @@ determineIncr acc prev next =
     if prev < next then 1
     else 0
 
-x :: List Int
-x = fromFoldable
+sample :: List Int
+sample = fromFoldable
   [ 199
   , 200
   , 208
