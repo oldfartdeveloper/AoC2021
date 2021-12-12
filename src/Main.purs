fa2b1e699b15e2cf431a7e5e3a7b6ea02d6503ef
oldfartdeveloper <scottnelsonsmith@gmail.com>
2021-12-12 12:42:37 -0800

    In progress...

--- src/Main.purs
+++ src/Main.purs
@@ -1,25 +1,63 @@
 module Main where
 
 import Prelude
-import Data.Array (catMaybes)
+import Data.Array (catMaybes, replicate, singleton)
 import Data.Foldable (foldl)
 import Data.Generic.Rep (class Generic)
 import Data.Int (binary, fromStringAs)
 import Data.Maybe (Maybe)
 import Data.Newtype (class Newtype, unwrap)
 import Data.Show.Generic (genericShow)
-import Data.String.Utils (lines)
+import Data.String.Common (joinWith)
+import Data.String.Utils (length, lines)
 import Effect (Effect)
 import Effect.Console (log)
-import Node.Encoding (Encoding(ASCII))
+import Node.Encoding (Encoding(UTF8))
 import Node.FS.Sync (readTextFile)
 
 {- | ? is the right answer!
 -}
 main :: Effect Unit
 main = do
-  contents <- readTextFile ASCII "src/sample.txt"
-  log $ show $ day3 $ getBits contents
+  contents <- readTextFile UTF8 "src/sample.txt"
+  log $ show $ day3 $ getBits $ {- setMask $ -}  lines contents
+
+-- setMask :: Array String -> State
+-- setMask as =
+--   let maskStr = head as
+--   in
+
+maskFromStr :: String -> Int
+maskFromStr s =
+  let
+    len = length s
+    mask = "1" <> (joinWith "" $ replicate (len - 1) $ singleton "0")
+  in
+    fromStringAs binary mask
+
+getBits :: Array String -> Array Int
+getBits deltas =
+  catMaybes $ map convertStringToBits $ deltas
+
+day3 :: Array Int -> Int
+day3 arr =
+  let
+    result = calculateBits arr
+  in
+    (unwrap result).gamma * (unwrap result).epsilon
+
+convertStringToBits :: String -> Maybe Int
+convertStringToBits line =
+  fromStringAs binary line
+
+newtype State = State
+  { bits :: Array Int
+  , screwMask :: Int
+  }
+
+derive instance Generic State _
+instance Show State where
+  show = genericShow
 
 newtype Diagnostic = Diagnostic
   { gamma :: Int
@@ -31,30 +69,18 @@ derive instance Generic Diagnostic _
 instance Show Diagnostic where
   show = genericShow
 
-day3 :: Array Int -> Int
-day3 arr =
-  let
-    result =
-      foldl
-        ( \(Diagnostic d) bit ->
-            Diagnostic
-              { gamma: d.gamma
-              , epsilon: d.epsilon
-              }
-        )
-        ( Diagnostic
-            { gamma: 0
-            , epsilon: 0
-            }
-        )
-        arr
-  in
-    (unwrap result).gamma * (unwrap result).epsilon
-
-convertStringToBits :: String -> Maybe Int
-convertStringToBits line =
-  fromStringAs binary line
-
-getBits :: String -> Array Int
-getBits deltas =
-  catMaybes $ map convertStringToBits $ lines deltas
+calculateBits :: Array Int -> Array Diagnostic
+calculateBits arr =
+  foldl
+    ( \(Diagnostic d) bits ->
+        Diagnostic
+          { gamma: d.gamma
+          , epsilon: d.epsilon
+          }
+    )
+    ( Diagnostic
+        { gamma: 0
+        , epsilon: 0
+        }
+    )
+    arr
