788d173f288052350c13ca506fe4c93ca72a06b4
oldfartdeveloper <scottnelsonsmith@gmail.com>
2022-03-28 05:00:40 -0700

    ProcessInput compiles but fails to parse =

--- day22/src/ProcessInput.purs
+++ day22/src/ProcessInput.purs
@@ -2,7 +2,7 @@ module ProcessInput where
 
 import Prelude
 
-import Data.Array ((..), init, length, tail)
+import Data.Array ((..), index, init, length, tail)
 import Data.Int (fromString)
 import Data.String.Common (split)
 import Data.String.Pattern
@@ -11,7 +11,7 @@ import Utils (unsafeJust)
 
 newtype SpecLine = SpecLine
   { state :: State
-  , cuboid :: Array Cuboid
+  , cuboid :: Cuboid
   }
 
 data State
@@ -33,20 +33,25 @@ processInput inputText =
   map makeSpecLine $ getLines inputText
   where
   getLines :: String -> Array String
-  getLines = unsafeJust $ init $ unsafeJust $ tail
+  getLines text = unsafeJust $ init $ unsafeJust $ tail $ lines text
 
 makeSpecLine :: String -> SpecLine
-makeSpecLine s =
+makeSpecLine specLineStr =
   SpecLine
-    { state: getState ps
-    , cuboid: getCuboid ps
+    { state: getState $ ps 0
+    , cuboid: getCuboid $ ps 1
     }
   where
-  ps = split (Pattern " ") s
-  getState s'
-    | s' == "on" = On
+  ps :: Int -> String
+  ps i = unsafeJust $ index (split (Pattern " ") specLineStr) i
+
+  getState :: String -> State
+  getState stateStr
+    | stateStr == "on" = On
     | otherwise = Off
-  getCuboid cs = map makeCuboid $ split "," cs
+
+  getCuboid :: String -> Cuboid
+  getCuboid cs = makeCuboid $ split (Pattern ",") cs
     where
     makeCuboid :: Array String -> Cuboid
     makeCuboid dims =
@@ -57,14 +62,22 @@ makeSpecLine s =
         }
       where
       rangeEnds :: Int -> Range
-      rangeEnds i =
+      rangeEnds dimSelector =
         Range
-          { start: ends [ 0 ]
-          , end: ends [ 1 ]
+          { start: unsafeJust $ index ends 0
+          , end: unsafeJust $ index ends 1
           }
         where
-        ends = getEnds i
+        ends = getEnds
 
-        getEnds :: Int -> Array Int
-        getEnds i' = map fromString (split (Pattern "::") dims [ i' ])
+        getEnds :: Array Int
+        getEnds =
+          map parseInt $ (split (Pattern "::") $ dim)
+          where
+          dim :: String
+          dim = unsafeJust $ index dims dimSelector
+
+          parseInt :: String -> Int
+          parseInt =
+            unsafeJust <<< fromString
 
