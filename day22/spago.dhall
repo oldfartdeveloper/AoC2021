b527b73191186dc3e0576ff53adca1321513a9ae
oldfartdeveloper <scottnelsonsmith@gmail.com>
2022-03-28 03:25:49 -0700

    Almost have makeSpecLine compiling

--- day22/spago.dhall
+++ day22/spago.dhall
@@ -16,6 +16,7 @@ to generate this file without the comments in this block.
   , "console"
   , "effect"
   , "foldable-traversable"
+  , "integers"
   , "maybe"
   , "partial"
   , "prelude"
--- day22/src/ProcessInput.purs
+++ day22/src/ProcessInput.purs
@@ -2,21 +2,23 @@ module ProcessInput where
 
 import Prelude
 
-import Data.Array (init, length, tail)
+import Data.Array ((..), init, length, tail)
+import Data.Int (fromString)
+import Data.String.Common (split)
+import Data.String.Pattern
 import Data.String.Utils (lines)
 import Utils (unsafeJust)
 
 newtype SpecLine = SpecLine
   { state :: State
-  , cubes :: Array Cube
+  , cuboid :: Array Cuboid
   }
 
-newtype State = State
-  { on :: Boolean
-  , off :: Boolean
-  }
+data State
+  = On
+  | Off
 
-newtype Cube = Cube
+newtype Cuboid = Cuboid
   { x :: Range
   , y :: Range
   , z :: Range
@@ -28,5 +30,41 @@ newtype Range = Range
   }
 
 processInput inputText =
-  unsafeJust $ init $ unsafeJust $ tail $ lines inputText
+  map makeSpecLine $ getLines inputText
+  where
+  getLines :: String -> Array String
+  getLines = unsafeJust $ init $ unsafeJust $ tail
+
+makeSpecLine :: String -> SpecLine
+makeSpecLine s =
+  SpecLine
+    { state: getState ps
+    , cuboid: getCuboid ps
+    }
+  where
+  ps = split (Pattern " ") s
+  getState s'
+    | s' == "on" = On
+    | otherwise = Off
+  getCuboid cs = map makeCuboid $ split "," cs
+    where
+    makeCuboid :: Array String -> Cuboid
+    makeCuboid dims =
+      Cuboid
+        { x: rangeEnds 0
+        , y: rangeEnds 1
+        , z: rangeEnds 2
+        }
+      where
+      rangeEnds :: Int -> Range
+      rangeEnds i =
+        Range
+          { start: ends [ 0 ]
+          , end: ends [ 1 ]
+          }
+        where
+        ends = getEnds i
+
+        getEnds :: Int -> Array Int
+        getEnds i' = map fromString (split (Pattern "::") dims [ i' ])
 
